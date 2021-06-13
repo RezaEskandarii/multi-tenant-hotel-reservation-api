3b6785275868284433a95837802310787cc7fe33
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-06-13 20:56:31 +0430

    feat: add some middlewares to echo router

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module hotel-reservation
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/echo/v4 v4.3.0 // indirect
 	github.com/lib/pq v1.10.2 // indirect
+	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.10
--- internal/bootstrap/bootstrap.go
+++ internal/bootstrap/bootstrap.go
@@ -3,6 +3,7 @@ package bootstrap
 import (
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"hotel-reservation/internal/registery"
 	"hotel-reservation/pkg/application_loger"
 	"hotel-reservation/pkg/database"
@@ -34,6 +35,10 @@ func Run(port int) error {
 	portStr := fmt.Sprintf(":%d", port)
 
 	e := echo.New()
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
 	router := e.Group("/v1")
 
 	registery.RegisterServices(db, router)
