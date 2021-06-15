605fc483ea64b573c5b40c971e048e3dfdaf5eac
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-06-15 20:15:43 +0430

    feat: register city handler

--- internal/registery/service_registery.go
+++ internal/registery/service_registery.go
@@ -10,11 +10,13 @@ import (
 // services
 var (
 	countryService = *services.NewCountryService()
+	cityService    = *services.NewCityService()
 )
 
 // handlers
 var (
 	countryHandler = handlers.CountryHandler{}
+	cityHandler    = handlers.CityHandler{}
 )
 
 func RegisterServices(db *gorm.DB, router *echo.Group) {
@@ -22,4 +24,9 @@ func RegisterServices(db *gorm.DB, router *echo.Group) {
 	countriesRouter := router.Group("/countries")
 	countryService.Repository.DB = db
 	countryHandler.Register(countriesRouter, countryService)
+
+	citiesRouter := router.Group("/cities")
+	cityService.Repository.DB = db
+	cityHandler.Register(citiesRouter, cityService)
+
 }
