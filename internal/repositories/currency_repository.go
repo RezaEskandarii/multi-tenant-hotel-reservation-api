3584b2682d2e4cadeda0348ee96700b1792c6173
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-06-24 18:24:03 +0430

    feat: add currency repository

---
+++ internal/repositories/currency_repository.go
@@ -0,0 +1,59 @@
+package repositories
+
+import (
+	"gorm.io/gorm"
+	"hotel-reservation/internal/commons"
+	"hotel-reservation/internal/dto"
+	"hotel-reservation/internal/models"
+	"hotel-reservation/pkg/application_loger"
+)
+
+type CurrencyRepository struct {
+	DB *gorm.DB
+}
+
+func NewCurrencyRepository(db *gorm.DB) *CurrencyRepository {
+	return &CurrencyRepository{
+		DB: db,
+	}
+}
+
+func (r *CurrencyRepository) Create(country *models.Currency) (*models.Currency, error) {
+
+	if tx := r.DB.Create(&country); tx.Error != nil {
+		application_loger.LogError(tx.Error)
+		return nil, tx.Error
+	}
+
+	return country, nil
+}
+
+func (r *CurrencyRepository) Update(country *models.Currency) (*models.Currency, error) {
+
+	if tx := r.DB.Updates(&country); tx.Error != nil {
+		application_loger.LogError(tx.Error)
+		return nil, tx.Error
+	}
+
+	return country, nil
+}
+
+func (r *CurrencyRepository) Find(id uint64) (*models.Currency, error) {
+
+	model := models.Currency{}
+	if tx := r.DB.Where("id=?", id).Find(&model); tx.Error != nil {
+		application_loger.LogError(tx.Error)
+		return nil, tx.Error
+	}
+
+	if model.Id == 0 {
+		return nil, nil
+	}
+
+	return &model, nil
+}
+
+func (r *CurrencyRepository) FindAll(input *dto.PaginationInput) (*commons.PaginatedList, error) {
+
+	return finAll(&models.Currency{}, r.DB, input)
+}
