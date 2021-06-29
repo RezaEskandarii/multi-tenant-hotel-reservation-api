c99d2b88037608e3eef116f959685ac61290e4c5
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-06-29 20:31:00 +0430

    doc: add document to province service functions

--- internal/services/province_service.go
+++ internal/services/province_service.go
@@ -11,30 +11,36 @@ type ProvinceService struct {
 	Repository repositories.ProvinceRepository
 }
 
+// NewProvinceService returns new ProvinceService
 func NewProvinceService() *ProvinceService {
 	return &ProvinceService{}
 }
 
+// Create creates new province.
 func (s *ProvinceService) Create(Province *models.Province) (*models.Province, error) {
 
 	return s.Repository.Create(Province)
 }
 
+// Update updates province.
 func (s *ProvinceService) Update(Province *models.Province) (*models.Province, error) {
 
 	return s.Repository.Update(Province)
 }
 
+// Find returns province and if it does not find the province, it returns nil.
 func (s *ProvinceService) Find(id uint64) (*models.Province, error) {
 
 	return s.Repository.Find(id)
 }
 
+// FindAll returns paginates list of provinces.
 func (s *ProvinceService) FindAll(input *dto.PaginationInput) (*commons.PaginatedList, error) {
 
 	return s.Repository.FindAll(input)
 }
 
+// GetCities returns the list of cities that belong to the given province ID.
 func (s *ProvinceService) GetCities(ProvinceId uint64) ([]*models.City, error) {
 
 	return s.Repository.GetCities(ProvinceId)
