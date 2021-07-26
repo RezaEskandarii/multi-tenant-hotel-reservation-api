53c8fa0c8bfdbe5d291c5b7a0280c2fa37520b4d
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-07-26 21:04:07 +0430

    refactor: remove some extra error codes

--- internal/commons/status_codes.go
+++ internal/commons/status_codes.go
@@ -1,9 +1,7 @@
 package commons
 
 const (
-	BadRequest          = 400
-	Ok                  = 200
-	InternalServerError = 500
-	ReservationConflict = 700
-	RoomBusy            = 701
+	ResidenceTypeHasResidence = 800
+	ReservationConflict       = 700
+	RoomBusy                  = 701
 )
