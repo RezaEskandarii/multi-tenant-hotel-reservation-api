02376e7af40f0239ee468f20d431807c2a6859ea
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-07-29 19:23:23 +0430

    test: refactor country_service_test

--- internal/services/country_service_test.go
+++ internal/services/country_service_test.go
@@ -15,37 +15,77 @@ var (
 		Page:    1,
 		PerPage: 20,
 	}
+	countryService = *NewCountryService()
 )
 
-func TestCountryService(t *testing.T) {
-
-	id := uint64(1000000001)
-	countryService := *NewCountryService()
+func init() {
 	countryService.Repository = repositories.NewCountryRepository(db)
+}
 
-	t.Run("Create", func(t *testing.T) {
-		c := &models.Country{
-			Name:  "Iran",
-			Alias: "IRI",
-		}
-		c.Id = id
-		result, err := countryService.Create(c)
-		assert.Nil(t, err)
-		assert.NotNil(t, result)
-		assert.Equal(t, result.Name, c.Name)
-	})
+func TestCanCreateNewCountry(t *testing.T) {
 
-	t.Run("Find", func(t *testing.T) {
-		result, err := countryService.Find(id)
-		assert.Nil(t, err)
-		assert.NotNil(t, result)
-		assert.Equal(t, result.Id, id)
-		assert.NotEqual(t, result.Id, 0)
-	})
+	country1 := models.Country{
+		Name:  "Iran",
+		Alias: "IRI",
+	}
 
-	t.Run("FindAll", func(t *testing.T) {
-		result, err := countryService.FindAll(&pagination)
-		assert.Nil(t, err)
-		assert.NotNil(t, result)
-	})
+	country2, err := countryService.Create(&country1)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, country2)
+	assert.Equal(t, country1.Id, country2.Id)
+	assert.NotEqual(t, country2, 0)
+
+}
+
+func TestCanFindCountry(t *testing.T) {
+
+	country1 := models.Country{
+		Name:  "Canada",
+		Alias: "Ca",
+	}
+
+	country2, err := countryService.Create(&country1)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, country2)
+	assert.NotEqual(t, country2, 0)
+
+	country3, err := countryService.Find(country2.Id)
+	assert.Nil(t, err)
+	assert.NotNil(t, country3)
+	assert.NotEqual(t, country3.Id, 0)
+	assert.Equal(t, country2.Name, country3.Name)
+}
+
+func TestCanUpdateCountry(t *testing.T) {
+	country1 := models.Country{
+		Name:  "Canada",
+		Alias: "Ca",
+	}
+	country2, err := countryService.Create(&country1)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, country2)
+	assert.NotEqual(t, country2, 0)
+
+	country2.Name = "Iran"
+	country2.Alias = "IRI"
+
+	country3, err := countryService.Update(country2)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, country3)
+	assert.NotEqual(t, country3.Id, 0)
+	assert.Equal(t, country2.Name, country3.Name)
+	assert.Equal(t, country2.Alias, country3.Alias)
+	assert.Equal(t, country3.Name, "Iran")
+	assert.Equal(t, country3.Alias, "IRI")
+}
+
+func TestCanFindAllCountries(t *testing.T) {
+	list, err := countryService.FindAll(&pagination)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, list)
 }
