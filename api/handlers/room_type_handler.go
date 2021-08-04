1c0407d99fa7b5ecaf9088add448d6f5b32fc5d9
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-08-04 23:18:42 +0430

    feat: add room type handler

---
+++ api/handlers/room_type_handler.go
@@ -0,0 +1,196 @@
+package handlers
+
+import (
+	"github.com/labstack/echo/v4"
+	middlewares2 "hotel-reservation/api/middlewares"
+	"hotel-reservation/internal/commons"
+	"hotel-reservation/internal/dto"
+	"hotel-reservation/internal/message_keys"
+	"hotel-reservation/internal/models"
+	"hotel-reservation/internal/services"
+	"hotel-reservation/internal/utils"
+	"hotel-reservation/pkg/applogger"
+	"hotel-reservation/pkg/translator"
+	"net/http"
+)
+
+// RoomTypeHandler Province endpoint handler
+type RoomTypeHandler struct {
+	Router     *echo.Group
+	Service    services.RoomTypeService
+	translator *translator.Translator
+}
+
+func (handler *RoomTypeHandler) Register(router *echo.Group, service *services.RoomTypeService, translator *translator.Translator) {
+	handler.Router = router
+	handler.Service = *service
+	handler.translator = translator
+
+	handler.Router.POST("", handler.create)
+	handler.Router.PUT("/:id", handler.update)
+	handler.Router.GET("/:id", handler.find)
+	handler.Router.DELETE("/:id", handler.delete)
+	handler.Router.GET("", handler.findAll, middlewares2.PaginationMiddleware)
+}
+
+func (handler *RoomTypeHandler) create(c echo.Context) error {
+
+	model := &models.RoomType{}
+	lang := c.Request().Header.Get(acceptLanguage)
+
+	if err := c.Bind(&model); err != nil {
+
+		applogger.LogError(err.Error())
+
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			Data:         nil,
+			ResponseCode: http.StatusBadRequest,
+			Message:      message_keys.BadRequest,
+		})
+	}
+
+	result, err := handler.Service.Create(model)
+
+	if err != nil {
+
+		applogger.LogError(err.Error())
+
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			Data:         nil,
+			ResponseCode: http.StatusBadRequest,
+			Message:      "",
+		})
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data:         result,
+		ResponseCode: http.StatusOK,
+		Message:      handler.translator.Localize(lang, message_keys.Created),
+	})
+}
+
+func (handler *RoomTypeHandler) update(c echo.Context) error {
+
+	lang := c.Request().Header.Get(acceptLanguage)
+	id, err := utils.ConvertToUint(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+
+	result, err := handler.Service.Find(id)
+	if err != nil {
+
+		applogger.LogError(err.Error())
+
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			ResponseCode: http.StatusBadRequest,
+			Message:      handler.translator.Localize(lang, message_keys.BadRequest),
+		})
+	}
+
+	if result == nil || (result != nil && result.Id == 0) {
+		return c.JSON(http.StatusNotFound, commons.ApiResponse{
+			ResponseCode: http.StatusNotFound,
+			Message:      handler.translator.Localize(lang, message_keys.NotFound),
+		})
+	}
+
+	name := c.FormValue("name")
+	result.Name = name
+
+	updatedMode, err := handler.Service.Update(result)
+
+	if err != nil {
+
+		applogger.LogError(err.Error())
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data:         updatedMode,
+		ResponseCode: http.StatusOK,
+	})
+}
+
+func (handler *RoomTypeHandler) find(c echo.Context) error {
+
+	lang := c.Request().Header.Get(acceptLanguage)
+	id, err := utils.ConvertToUint(c.Param("id"))
+
+	if err != nil {
+
+		applogger.LogError(err.Error())
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+
+	result, err := handler.Service.Find(id)
+
+	if err != nil {
+
+		applogger.LogError(err.Error())
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			ResponseCode: http.StatusBadRequest,
+			Message:      handler.translator.Localize(lang, message_keys.BadRequest),
+		})
+	}
+
+	if result == nil || (result != nil && result.Id == 0) {
+		return c.JSON(http.StatusNotFound, commons.ApiResponse{
+			ResponseCode: http.StatusNotFound,
+			Message:      handler.translator.Localize(lang, message_keys.NotFound),
+		})
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data:         result,
+		ResponseCode: http.StatusOK,
+	})
+}
+
+func (handler *RoomTypeHandler) findAll(c echo.Context) error {
+
+	paginationInput := c.Get(paginationInput).(*dto.PaginationInput)
+
+	list, err := handler.Service.FindAll(paginationInput)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data:         list,
+		ResponseCode: http.StatusOK,
+		Message:      "",
+	})
+}
+
+func (handler *RoomTypeHandler) delete(c echo.Context) error {
+
+	id, err := utils.ConvertToUint(c.Param("id"))
+	lang := c.Request().Header.Get(acceptLanguage)
+
+	if err != nil {
+
+		applogger.LogError(err.Error())
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			ResponseCode: http.StatusBadRequest,
+			Message:      handler.translator.Localize(lang, message_keys.BadRequest),
+		})
+	}
+
+	err = handler.Service.Delete(id)
+
+	if err != nil {
+
+		applogger.LogError(err.Error())
+		return c.JSON(http.StatusConflict, commons.ApiResponse{
+			ResponseCode: http.StatusConflict,
+			Message:      handler.translator.Localize(lang, err.Error()),
+		})
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		ResponseCode: http.StatusOK,
+		Message:      handler.translator.Localize(lang, message_keys.Deleted),
+	})
+}
