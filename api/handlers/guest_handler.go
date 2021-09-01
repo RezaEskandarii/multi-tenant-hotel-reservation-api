84820a2ac8be887ff304753b4aaf33594d285eb3
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-09-01 20:31:39 +0430

    feat: implement guests handler

--- api/handlers/guest_handler.go
+++ api/handlers/guest_handler.go
@@ -7,6 +7,7 @@ import (
 	"hotel-reservation/internal/message_keys"
 	"hotel-reservation/internal/models"
 	"hotel-reservation/internal/services"
+	"hotel-reservation/internal/utils"
 	"hotel-reservation/pkg/applogger"
 	"hotel-reservation/pkg/translator"
 	"net/http"
@@ -35,6 +36,8 @@ func (handler *GuestHandler) create(c echo.Context) error {
 
 	model := models.Guest{}
 
+	lang := c.Request().Header.Get(acceptLanguage)
+
 	if err := c.Bind(&model); err != nil {
 
 		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
@@ -43,5 +46,97 @@ func (handler *GuestHandler) create(c echo.Context) error {
 		})
 	}
 
-	panic("not implemetend !!!")
+	if _, err := handler.Service.Create(&model); err != nil {
+
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			Message: handler.translator.Localize(lang, err.Error()),
+		})
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data:    model,
+		Message: handler.translator.Localize(lang, message_keys.Created),
+	})
+}
+
+func (handler *GuestHandler) update(c echo.Context) error {
+
+	model := models.Guest{}
+	lang := c.Request().Header.Get(acceptLanguage)
+
+	id, _ := utils.ConvertToUint(c.Get("id"))
+
+	guest, _ := handler.Service.Find(id)
+
+	if guest == nil || (guest != nil && guest.Id == 0) {
+
+		return c.JSON(http.StatusNotFound, commons.ApiResponse{
+			Message: handler.translator.Localize(lang, message_keys.NotFound),
+		})
+	}
+
+	if err := c.Bind(&model); err != nil {
+
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			ResponseCode: http.StatusBadRequest,
+			Message:      handler.translator.Localize(c.Request().Header.Get(acceptLanguage), message_keys.BadRequest),
+		})
+	}
+
+	if _, err := handler.Service.Update(&model); err != nil {
+
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			Message: handler.translator.Localize(lang, err.Error()),
+		})
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data:    model,
+		Message: handler.translator.Localize(lang, message_keys.Updated),
+	})
+}
+
+func (handler *GuestHandler) find(c echo.Context) error {
+
+	lang := c.Request().Header.Get(acceptLanguage)
+	id, _ := utils.ConvertToUint(c.Get("id"))
+
+	guest, _ := handler.Service.Find(id)
+
+	if guest == nil || (guest != nil && guest.Id == 0) {
+
+		return c.JSON(http.StatusNotFound, commons.ApiResponse{
+			Message: handler.translator.Localize(lang, message_keys.NotFound),
+		})
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data: guest,
+	})
+}
+
+func (handler *GuestHandler) findAll(c echo.Context) error {
+
+	lang := c.Request().Header.Get(acceptLanguage)
+
+	page, _ := utils.ConvertToUint(c.Param("page"))
+	perPage, _ := utils.ConvertToUint(c.Param("perPage"))
+
+	input := &dto.PaginationInput{
+		Page:    int(page),
+		PerPage: int(perPage),
+	}
+
+	result, err := handler.Service.FindAll(input)
+
+	if err != nil {
+
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			Message: handler.translator.Localize(lang, err.Error()),
+		})
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data: result,
+	})
 }
