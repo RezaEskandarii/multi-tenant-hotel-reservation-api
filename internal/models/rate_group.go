b9b70660ec2c0d9ca9e2f81798b360db07be99dd
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-09-20 19:18:33 +0430

    feat: add rate group model

---
+++ internal/models/rate_group.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"github.com/asaskevich/govalidator"
+	"gorm.io/gorm"
+)
+
+// RateGroup is struct for rating group of reservations rate price.
+type RateGroup struct {
+	BaseModel
+	Name        string    `json:"name" valid:"required"`
+	Description string    `json:"description"`
+	ResidenceId uint64    `json:"residence_id" valid:"required"`
+	Residence   Residence `json:"residence"`
+}
+
+func (r *RateGroup) Validate() (bool, error) {
+
+	return govalidator.ValidateStruct(r)
+}
+
+func (r *RateGroup) BeforeCreate(tx *gorm.DB) error {
+	_, err := r.Validate()
+
+	if err != nil {
+		tx.AddError(err)
+		return err
+	}
+
+	return nil
+}
