86ec60bd5b650328550eda4f89136b28ccd8aba6
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-10-04 19:41:55 +0330

    feat: add validator to rate_code model

--- internal/models/rate_code.go
+++ internal/models/rate_code.go
@@ -1,15 +1,33 @@
 package models
 
+import (
+	"github.com/asaskevich/govalidator"
+	"gorm.io/gorm"
+)
+
 type RateCode struct {
 	BaseModel
-	Name        string    `json:"name"`
-	Residence   Residence `json:"residence"`
-	ResidenceId uint64    `json:"residence_id"`
-	Currency    Currency  `json:"currency"`
-	CurrencyId  uint64    `json:"currency_id"`
-	RateGroup   RateGroup `json:"rate_group"`
-	RateGroupId uint64    `json:"rate_group_id"`
-	Guest       Guest     `json:"guest"`
-	GuestIs     uint64    `json:"guest_is"`
+	Name        string    `json:"name"  valid:"required"`
+	Residence   Residence `json:"residence"  valid:"-"`
+	ResidenceId uint64    `json:"residence_id"  valid:"required"`
+	Currency    Currency  `json:"currency"  valid:"-"`
+	CurrencyId  uint64    `json:"currency_id" valid:"required"`
+	RateGroup   RateGroup `json:"rate_group" valid:"-"`
+	RateGroupId uint64    `json:"rate_group_id"  valid:"required"`
+	Guest       Guest     `json:"guest"  valid:"-"`
+	GuestId     uint64    `json:"guest_id"  valid:"required"`
 	Status      bool      `json:"status"`
+}
+
+func (r *RateCode) Validate() (bool, error) {
+
+	return govalidator.ValidateStruct(r)
+}
+
+func (r *RateCode) BeforeCreate(tx *gorm.DB) error {
+	if _, err := r.Validate(); err != nil {
+		tx.AddError(err)
+		return err
+	}
+	return nil
 }
