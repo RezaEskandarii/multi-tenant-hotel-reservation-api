4c1a9b9a3247d85aff25f30cce3d323c1ee4a9d6
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-10-09 19:01:47 +0330

    feat: implement rate_code_repository functions

--- internal/repositories/rate_code_repository.go
+++ internal/repositories/rate_code_repository.go
@@ -1,6 +1,11 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"hotel-reservation/internal/commons"
+	"hotel-reservation/internal/dto"
+	"hotel-reservation/internal/models"
+)
 
 type RateCodeRepository struct {
 	DB *gorm.DB
@@ -10,4 +15,48 @@ type RateCodeRepository struct {
 func NewRateCodeRepository(db *gorm.DB) *RateCodeRepository {
 
 	return &RateCodeRepository{DB: db}
+}
+
+func (r *RateCodeRepository) Create(model *models.RateCode) (*models.RateCode, error) {
+
+	if tx := r.DB.Create(&model); tx.Error != nil {
+		return nil, tx.Error
+	}
+	return model, nil
+}
+
+func (r *RateCodeRepository) Update(model *models.RateCode) (*models.RateCode, error) {
+
+	if tx := r.DB.Updates(&model); tx.Error != nil {
+		return nil, tx.Error
+	}
+	return model, nil
+}
+
+func (r *RateCodeRepository) Find(id uint64) (*models.RateCode, error) {
+
+	model := models.RateCode{}
+
+	if tx := r.DB.Where("id=?", id).Find(&model); tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	if model.Id == 0 {
+		return nil, nil
+	}
+	return &model, nil
+}
+
+func (r *RateCodeRepository) FindAll(input *dto.PaginationInput) (*commons.PaginatedList, error) {
+
+	return finAll(&models.RateCode{}, r.DB, input)
+}
+
+func (r RateCodeRepository) Delete(id uint64) error {
+
+	if query := r.DB.Model(&models.RateCode{}).Where("id=?", id).Delete(&models.RateCode{}); query.Error != nil {
+
+		return query.Error
+	}
+	return nil
 }
