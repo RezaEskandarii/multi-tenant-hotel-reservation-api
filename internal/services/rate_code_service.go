edc3377d86513c2e87d655b088d4fb0102c20e22
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-10-10 20:39:15 +0330

    feat: add rate_code_service

---
+++ internal/services/rate_code_service.go
@@ -0,0 +1,47 @@
+package services
+
+import (
+	"hotel-reservation/internal/commons"
+	"hotel-reservation/internal/dto"
+	"hotel-reservation/internal/models"
+	"hotel-reservation/internal/repositories"
+)
+
+type RateCodeService struct {
+	Repository *repositories.RateCodeRepository
+}
+
+// NewRateCodeService returns new RateCodeService
+func NewRateCodeService() *RateCodeService {
+	return &RateCodeService{}
+}
+
+// Create creates new RateCode.
+func (s *RateCodeService) Create(model *models.RateCode) (*models.RateCode, error) {
+
+	return s.Repository.Create(model)
+}
+
+// Update updates RateCode.
+func (s *RateCodeService) Update(model *models.RateCode) (*models.RateCode, error) {
+
+	return s.Repository.Update(model)
+}
+
+// Find returns RateCode and if it does not find the RateCode, it returns nil.
+func (s *RateCodeService) Find(id uint64) (*models.RateCode, error) {
+
+	return s.Repository.Find(id)
+}
+
+// FindAll returns paginates list of RateCodes.
+func (s *RateCodeService) FindAll(input *dto.PaginationInput) (*commons.PaginatedList, error) {
+
+	return s.Repository.FindAll(input)
+}
+
+// Delete removes RateCode  by given id.
+func (s *RateCodeService) Delete(id uint64) error {
+
+	return s.Repository.Delete(id)
+}
