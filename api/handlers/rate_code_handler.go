641b0a75cc8f527482afaa6f0e4b9961b1ef873f
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-10-13 19:22:22 +0330

    feat: add ratCodeHandler's functions

--- api/handlers/rate_code_handler.go
+++ api/handlers/rate_code_handler.go
@@ -2,10 +2,16 @@ package handlers
 
 import (
 	"github.com/labstack/echo/v4"
+	middlewares2 "hotel-reservation/api/middlewares"
+	"hotel-reservation/internal/commons"
 	"hotel-reservation/internal/dto"
+	"hotel-reservation/internal/message_keys"
+	"hotel-reservation/internal/models"
 	"hotel-reservation/internal/services"
+	"hotel-reservation/internal/utils"
 	"hotel-reservation/pkg/applogger"
 	"hotel-reservation/pkg/translator"
+	"net/http"
 )
 
 // RateCodeHandler RateCode endpoint handler
@@ -22,6 +28,181 @@ func (handler *RateCodeHandler) Register(input *dto.HandlerInput, service *servi
 	handler.translator = input.Translator
 	handler.logger = input.Logger
 
-	//routeCode := handler.Router.Group("/rate-Codes")
+	routeGroup := handler.Router.Group("/rate-groups")
 
+	routeGroup.POST("", handler.create)
+	routeGroup.PUT("/:id", handler.update)
+	routeGroup.GET("/:id", handler.find)
+	routeGroup.DELETE("/:id", handler.delete)
+	routeGroup.GET("", handler.findAll, middlewares2.PaginationMiddleware)
+}
+
+func (handler *RateCodeHandler) create(c echo.Context) error {
+
+	model := &models.RateCode{}
+
+	lang := c.Request().Header.Get(acceptLanguage)
+
+	if err := c.Bind(&model); err != nil {
+
+		handler.logger.LogError(err.Error())
+
+		return c.JSON(http.StatusBadRequest,
+			commons.ApiResponse{
+				Data:         nil,
+				ResponseCode: http.StatusBadRequest,
+				Message:      handler.translator.Localize(lang, message_keys.BadRequest),
+			})
+	}
+
+	output, err := handler.Service.Create(model)
+
+	if err != nil {
+
+		handler.logger.LogError(err.Error())
+
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			ResponseCode: http.StatusBadRequest,
+			Message:      err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+		ResponseCode: http.StatusOK,
+		Message:      handler.translator.Localize(lang, message_keys.Created),
+		Data:         output,
+	})
+}
+
+func (handler *RateCodeHandler) update(c echo.Context) error {
+
+	id, err := utils.ConvertToUint(c.Param("id"))
+	if err != nil {
+
+		handler.logger.LogError(err.Error())
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+	model, err := handler.Service.Find(id)
+
+	lang := c.Request().Header.Get(acceptLanguage)
+
+	if err != nil {
+
+		handler.logger.LogError(err.Error())
+
+		return c.JSON(http.StatusInternalServerError, commons.ApiResponse{
+			ResponseCode: http.StatusInternalServerError,
+			Message:      handler.translator.Localize(lang, message_keys.InternalServerError),
+		})
+	}
+
+	if model == nil {
+		return c.JSON(http.StatusNotFound, commons.ApiResponse{
+			Data:         nil,
+			ResponseCode: http.StatusNotFound,
+			Message:      handler.translator.Localize(lang, message_keys.NotFound),
+		})
+	}
+
+	if err := c.Bind(&model); err != nil {
+
+		handler.logger.LogError(err.Error())
+		return c.JSON(http.StatusBadRequest, nil)
+
+	}
+
+	if output, err := handler.Service.Update(model); err == nil {
+
+		return c.JSON(http.StatusOK, commons.ApiResponse{
+			Data:         output,
+			ResponseCode: http.StatusOK,
+			Message:      handler.translator.Localize(lang, message_keys.Updated),
+		})
+	} else {
+
+		handler.logger.LogError(err.Error())
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+}
+
+func (handler *RateCodeHandler) find(c echo.Context) error {
+	id, err := utils.ConvertToUint(c.Param("id"))
+	if err != nil {
+		handler.logger.LogError(err.Error())
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+	model, err := handler.Service.Find(id)
+	lang := c.Request().Header.Get(acceptLanguage)
+
+	if err != nil {
+
+		handler.logger.LogError(err.Error())
+
+		return c.JSON(http.StatusInternalServerError, commons.ApiResponse{
+			ResponseCode: http.StatusInternalServerError,
+			Message:      handler.translator.Localize(lang, message_keys.InternalServerError),
+		})
+	}
+
+	if model == nil {
+		return c.JSON(http.StatusNotFound, commons.ApiResponse{
+			Data:         nil,
+			ResponseCode: http.StatusNotFound,
+			Message:      handler.translator.Localize(lang, message_keys.NotFound),
+		})
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data:         model,
+		ResponseCode: http.StatusOK,
+		Message:      "",
+	})
+}
+
+func (handler *RateCodeHandler) findAll(c echo.Context) error {
+
+	paginationInput := c.Get(paginationInput).(*dto.PaginationInput)
+
+	list, err := handler.Service.FindAll(paginationInput)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		Data:         list,
+		ResponseCode: http.StatusOK,
+		Message:      "",
+	})
+}
+
+func (handler *RateCodeHandler) delete(c echo.Context) error {
+
+	id, err := utils.ConvertToUint(c.Param("id"))
+	lang := c.Request().Header.Get(acceptLanguage)
+
+	if err != nil {
+
+		handler.logger.LogError(err.Error())
+		return c.JSON(http.StatusBadRequest, commons.ApiResponse{
+			ResponseCode: http.StatusBadRequest,
+			Message:      handler.translator.Localize(lang, message_keys.BadRequest),
+		})
+	}
+
+	err = handler.Service.Delete(id)
+
+	if err != nil {
+
+		handler.logger.LogError(err.Error())
+		return c.JSON(http.StatusConflict, commons.ApiResponse{
+			ResponseCode: http.StatusConflict,
+			Message:      handler.translator.Localize(lang, err.Error()),
+		})
+	}
+
+	return c.JSON(http.StatusOK, commons.ApiResponse{
+		ResponseCode: http.StatusOK,
+		Message:      handler.translator.Localize(lang, message_keys.Deleted),
+	})
 }
