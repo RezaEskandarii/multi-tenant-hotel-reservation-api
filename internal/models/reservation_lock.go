3b61dbd4160040df681a55830efb2ae87f093b9b
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-10-15 18:20:48 +0330

    feat: generate reservationKey's hash before create

--- internal/models/reservation_lock.go
+++ internal/models/reservation_lock.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"hotel-reservation/internal/utils"
+	"time"
+)
 
 type ReservationLock struct {
 	BaseModel
@@ -11,4 +16,10 @@ type ReservationLock struct {
 	StartTime  time.Time `json:"start_time"`
 	ExpireTime time.Time `json:"expire_time"`
 	LockKey    string    `json:"lock_key"`
+}
+
+func (r *ReservationLock) BeforeCreate(tx *gorm.DB) error {
+	strToHash := fmt.Sprintf("%d_%d_%d_%s", r.RoomId, r.GuestId, r.RateCodeId, r.StartTime)
+	r.LockKey = utils.GenerateSHA256(strToHash)
+	return nil
 }
