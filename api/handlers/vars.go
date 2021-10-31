39321a982eeb5b3ddffa881cccdc155dba614336
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-10-31 19:10:32 +0330

    doc: add comment

--- api/handlers/vars.go
+++ api/handlers/vars.go
@@ -2,6 +2,6 @@ package handlers
 
 var (
 	paginationInput = "paginationInput"
-	acceptLanguage  = "Accept-Language"
-	tenantId        = "X-TenantID"
+	acceptLanguage  = "Accept-Language" // accept language header.
+	tenantId        = "X-TenantID"      // this value uses in http header request as a selected business id.
 )
