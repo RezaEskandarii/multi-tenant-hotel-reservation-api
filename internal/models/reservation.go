db80da3b89cabf6ea5f94a5b3fa1ea4a3c44941d
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-11-01 20:03:31 +0330

    feat: add reservation type field to reservation model

--- internal/models/reservation.go
+++ internal/models/reservation.go
@@ -6,21 +6,29 @@ import (
 	"time"
 )
 
+type ReservationType string
+
+const (
+	Normal ReservationType = "Normal" // normal reservation.
+	Lock   ReservationType = "Lock"   // block a period of times to prevent reservation.
+)
+
 type Reservation struct {
 	BaseModel
-	Residence    Residence      `json:"residence" valid:"-"`
-	ResidenceId  uint64         `json:"residence_id" valid:"required"`
-	Supervisor   Guest          `json:"supervisor" valid:"-"`
-	SupervisorId uint64         `json:"supervisor_id" valid:"required"`
-	CheckinDate  time.Time      `json:"checkin_date" valid:"required"`
-	CheckoutDate time.Time      `json:"checkout_date" valid:"required"`
-	Room         Room           `json:"room" valid:"-"`
-	RoomId       uint64         `json:"room_id" valid:"required"`
-	RateCode     RateCode       `json:"rate_code" valid:"required"`
-	RateCodeId   uint64         `json:"rate_code_id" valid:"required"`
-	GuestCount   uint64         `json:"guest_count" valid:"required"`
-	Children     []*Reservation `json:"children" valid:"-"`
-	ParentId     uint64         `json:"parent_id" valid:"-"`
+	Residence       Residence       `json:"residence" valid:"-"`
+	ResidenceId     uint64          `json:"residence_id" valid:"required"`
+	Supervisor      Guest           `json:"supervisor" valid:"-"`
+	SupervisorId    uint64          `json:"supervisor_id" valid:"required"`
+	CheckinDate     time.Time       `json:"checkin_date" valid:"required"`
+	CheckoutDate    time.Time       `json:"checkout_date" valid:"required"`
+	Room            Room            `json:"room" valid:"-"`
+	RoomId          uint64          `json:"room_id" valid:"required"`
+	RateCode        RateCode        `json:"rate_code" valid:"required"`
+	RateCodeId      uint64          `json:"rate_code_id" valid:"required"`
+	GuestCount      uint64          `json:"guest_count" valid:"required"`
+	Children        []*Reservation  `json:"children" valid:"-"`
+	ParentId        uint64          `json:"parent_id" valid:"-"`
+	ReservationType ReservationType `json:"reservation_type"`
 }
 
 func (r *Reservation) Validate() (bool, error) {
@@ -29,6 +37,11 @@ func (r *Reservation) Validate() (bool, error) {
 }
 
 func (r *Reservation) BeforeCreate(tx *gorm.DB) error {
+
+	if r.ReservationType == "" {
+		r.ReservationType = Normal
+	}
+
 	if _, err := r.Validate(); err != nil {
 		tx.AddError(err)
 		return err
