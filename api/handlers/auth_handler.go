698156a2fa5aeb869b2273769f5fdd40665f304a
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-11-08 00:14:25 +0330

    feat: implement refreshToken function in auth_handler

--- api/handlers/auth_handler.go
+++ api/handlers/auth_handler.go
@@ -5,10 +5,12 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"os"
 	"reservation-api/internal/dto"
 	"reservation-api/internal/services"
 	"reservation-api/pkg/applogger"
 	"reservation-api/pkg/translator"
+	"strings"
 	"time"
 )
 
@@ -66,7 +68,7 @@ func (handler *AuthHandler) signin(c echo.Context) error {
 
 	// Declare the expiration time of the token
 	// here, we have kept it as 5 minutes
-	expirationTime := time.Now().Add(5 * time.Minute)
+	expirationTime := time.Now().Add(30 * time.Minute)
 	// Create the JWT claims, which includes the username and expiry time
 	claims := &Claims{
 		Username: creds.Username,
@@ -78,15 +80,12 @@ func (handler *AuthHandler) signin(c echo.Context) error {
 
 	// Declare the token with the algorithm used for signing, and the claims
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	// Create the JWT string
-	tokenString, err := token.SignedString([]byte("netdata.io"))
+
+	tokenString, err := token.SignedString([]byte(getJwtKey()))
 	if err != nil {
 		// If there is an error in creating the JWT return an internal server error
 		return c.JSON(http.StatusInternalServerError, nil)
 	}
-
-	// Finally, we set the client cookie for "token" as the JWT we just generated
-	// we also set an expiry time which is the same as the token itself
 
 	return c.JSON(http.StatusOK, TokenResponse{
 		ExpireAt:    expirationTime,
@@ -96,10 +95,48 @@ func (handler *AuthHandler) signin(c echo.Context) error {
 
 func (handler *AuthHandler) refreshToken(c echo.Context) error {
 
-	return nil
+	tknStr := c.Request().Header.Get("Authorization")
+	tknStr = strings.Trim(tknStr, " Bearer")
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return getJwtKey(), nil
+	})
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+
+	if !tkn.Valid {
+		return c.JSON(http.StatusUnauthorized, nil)
+	}
+
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+
+	// Now, create a new token for the current use, with a renewed expiration time
+	expirationTime := time.Now().Add(30 * time.Minute)
+	claims.ExpiresAt = expirationTime.Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(getJwtKey())
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+
+	return c.JSON(http.StatusOK, TokenResponse{
+		ExpireAt:    expirationTime,
+		AccessToken: tokenString,
+	})
 }
 
 func (handler *AuthHandler) logOut(c echo.Context) error {
 
 	return nil
+}
+
+func getJwtKey() string {
+	jwtKey, _ := os.LookupEnv("JWT_KEY")
+	return jwtKey
 }
