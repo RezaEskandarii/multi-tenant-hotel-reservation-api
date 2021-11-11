01d684f6cbb1fd289107ad6998dae6701d2d10dc
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-11-11 19:19:03 +0330

    feat: add audit repository

---
+++ internal/repositories/AuditRepository.go
@@ -0,0 +1,36 @@
+package repositories
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"reservation-api/internal/commons"
+	"reservation-api/internal/dto"
+	"reservation-api/internal/models"
+)
+
+type AuditRepository struct {
+	Completed chan bool
+	Data      chan interface{}
+	DB        *gorm.DB
+}
+
+func NewAuditRepository(Db *gorm.DB, completedCh chan bool, DataCh chan interface{}) *AuditRepository {
+	return &AuditRepository{
+		Completed: completedCh,
+		Data:      DataCh,
+		DB:        Db,
+	}
+}
+
+func (r *AuditRepository) Save(model *models.Audit) (*models.Audit, error) {
+	model.Data = fmt.Sprintf("%v", model.DataChannel)
+	if err := r.DB.Save(&model).Error; err != nil {
+		return nil, err
+	}
+	return model, nil
+}
+
+func (r *AuditRepository) FindAll(input *dto.PaginationInput) (*commons.PaginatedList, error) {
+
+	return finAll(&models.City{}, r.DB, input)
+}
