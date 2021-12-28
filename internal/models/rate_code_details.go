0e6e71d120a50eda6e2cd7c6615a993132585385
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2021-12-28 20:01:07 +0330

    feat: add rate_code_details model

---
+++ internal/models/rate_code_details.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type RateCodeDetail struct {
+	BaseModel
+	RateCode   RateCode   `json:"rate_code"`
+	RateCodeId uint64     `json:"rate_code_id"`
+	NightCount uint64     `json:"guest_count"`
+	DateStart  *time.Time `json:"date_start"`
+	DateEnd    *time.Time `json:"date_end"`
+
+	RatePrices []*RateCodeDetailPrice `json:"rate_prices"`
+}
+
+type RateCodeDetailPrice struct {
+	GuestCount       uint64         `json:"guest_count"`
+	Price            uint64         `json:"price"`
+	RateCodeDetail   RateCodeDetail `json:"rate_code_detail"`
+	RateCodeDetailId uint64         `json:"rate_code_detail_id"`
+}
