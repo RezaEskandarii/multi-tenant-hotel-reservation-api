4c5c1ab6ccacbcd4da9c60f8029c817932473e12
rezaesskandari@gmail.com <rezaesskandari@gmail.com>
2022-01-06 21:56:51 +0330

    refactor: add pointer to rate_code model relation

--- internal/models/rate_code.go
+++ internal/models/rate_code.go
@@ -7,13 +7,13 @@ import (
 
 type RateCode struct {
 	BaseModel
-	Name        string    `json:"name"  valid:"required"`
-	Hotel       Hotel     `json:"hotel"  valid:"-"`
-	HotelId     uint64    `json:"hotel_id"  valid:"required"`
-	Currency    Currency  `json:"currency"  valid:"-"`
-	CurrencyId  uint64    `json:"currency_id" valid:"required"`
-	RateGroup   RateGroup `json:"rate_group" valid:"-"`
-	RateGroupId uint64    `json:"rate_group_id"  valid:"required"`
+	Name        string     `json:"name"  valid:"required"`
+	Hotel       *Hotel     `json:"hotel"  valid:"-"`
+	HotelId     uint64     `json:"hotel_id"  valid:"required"`
+	Currency    *Currency  `json:"currency"  valid:"-"`
+	CurrencyId  uint64     `json:"currency_id" valid:"required"`
+	RateGroup   *RateGroup `json:"rate_group" valid:"-"`
+	RateGroupId uint64     `json:"rate_group_id"`
 	//Guest       Guest     `json:"guest"  valid:"-"`
 	//GuestId     uint64    `json:"guest_id"  valid:"required"`
 	Status bool `json:"status"`
