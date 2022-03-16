56d9b38e58b5a077d386191a61383cc764600f56
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2022-03-16 19:26:24 +0330

    feat: add VerifyToken function to

--- internal/services/domain_services/auth_service.go
+++ internal/services/domain_services/auth_service.go
@@ -2,6 +2,7 @@ package domain_services
 
 import (
 	"errors"
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"reservation-api/internal/commons"
 	"reservation-api/internal/config"
@@ -14,7 +15,12 @@ type Credentials struct {
 }
 
 type Claims struct {
-	Username string `json:"username"`
+	Username    string `json:"username"`
+	Email       string `json:"email"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Address     string `json:"address"`
+	PhoneNumber string `json:"phone_number"`
 	jwt.StandardClaims
 }
 
@@ -23,6 +29,11 @@ type AuthService struct {
 	Config      *config.Config
 }
 
+var (
+	InvalidTokenError = errors.New("token is invalid")
+)
+
+// NewAuthService returns new instance of auth service.
 func NewAuthService(service *UserService, cfg *config.Config) *AuthService {
 	return &AuthService{
 		UserService: service,
@@ -30,6 +41,8 @@ func NewAuthService(service *UserService, cfg *config.Config) *AuthService {
 	}
 }
 
+// SignIn finds user with given username and password and
+// generates JWT token for user.
 func (s AuthService) SignIn(username, password string) (error, *commons.TokenResponse) {
 
 	// Get the expected password from our in memory map
@@ -44,7 +57,12 @@ func (s AuthService) SignIn(username, password string) (error, *commons.TokenRes
 	expirationTime := time.Now().Add(time.Duration(s.Config.Authentication.TokenAliveTime) * time.Minute)
 	// Create the JWT claims, which includes the username and expiry time
 	claims := &Claims{
-		Username: user.Username,
+		Username:    user.Username,
+		Email:       user.Email,
+		FirstName:   user.FirstName,
+		LastName:    user.LastName,
+		Address:     user.Address,
+		PhoneNumber: user.PhoneNumber,
 		StandardClaims: jwt.StandardClaims{
 			// In JWT, the expiry time is expressed as unix milliseconds
 			ExpiresAt: expirationTime.Unix(),
@@ -96,4 +114,47 @@ func (s *AuthService) RefreshToken(tokenStr string) (error, *commons.TokenRespon
 		ExpireAt:    expirationTime,
 		AccessToken: result,
 	}
+}
+
+func (s *AuthService) VerifyToken(jwtToken string) (error, *Claims) {
+
+	token, _ := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("uexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(s.Config.Authentication.JwtKey), nil
+	})
+
+	if token == nil {
+		return errors.New("token is null"), nil
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+
+		username := claims["username"]
+		user, err := s.UserService.FindByUsername(fmt.Sprintf("%s", username))
+
+		if err != nil {
+			return err, nil
+		}
+
+		if user == nil {
+			return InvalidTokenError, nil
+		}
+
+		return nil, &Claims{
+			Username:    fmt.Sprintf("%s", claims["username"]),
+			Email:       fmt.Sprintf("%s", claims["email"]),
+			FirstName:   fmt.Sprintf("%s", claims["firstname"]),
+			LastName:    fmt.Sprintf("%s", claims["lastname"]),
+			Address:     fmt.Sprintf("%s", claims["address"]),
+			PhoneNumber: fmt.Sprintf("%s", claims["phonenumber"]),
+		}
+
+	} else {
+		return InvalidTokenError, nil
+	}
+
 }
