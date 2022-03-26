a10ade7bb57f6f94e2ffd39aeaeb6cf7349a2a86
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2022-03-26 20:05:29 +0430

    test: unit test for translation service

---
+++ pkg/translator/localizer_test.go
@@ -0,0 +1,36 @@
+package translator
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type TranslationTestCase struct {
+	input string
+	want  string
+}
+
+func TestCanLocalizeMessage(t *testing.T) {
+
+	testCases := []TranslationTestCase{
+		{
+			input: "AppName",
+			want:  "reservation-api",
+		},
+		{
+			input: "Errors.BadRequest",
+			want:  "BadRequest",
+		},
+	}
+
+	translationService := New()
+	assert.NotNil(t, translationService)
+
+	for _, item := range testCases {
+		result := translationService.Localize("en", item.input)
+		assert.NotNil(t, result)
+		assert.NotEqual(t, result, "")
+		assert.Equal(t, result, item.want)
+	}
+
+}
