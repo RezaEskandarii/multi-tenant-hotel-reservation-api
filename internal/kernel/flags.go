4ba4136f670926dc241ed275e012814c97e26e3b
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2022-06-30 20:20:15 +0430

    refactor: add transaction to database migrator

--- internal/kernel/flags.go
+++ internal/kernel/flags.go
@@ -13,12 +13,19 @@ func loadFlags() {
 	var seed = false
 	flag.BoolVar(&migrate, "migrate", false, "migrate structs and struct changes to database.")
 	flag.BoolVar(&seed, "seed", false, "seed default data from json file into database.")
-
 	flag.Parse()
+
+	var tx = db.Begin()
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
 
 	if migrate {
 		fmt.Println("migration started...")
-		if err := database.Migrate(db); err != nil {
+		if err := database.Migrate(tx); err != nil {
 			logger.LogError(err)
 			os.Exit(1)
 		}
@@ -26,9 +33,13 @@ func loadFlags() {
 
 	if seed {
 		fmt.Println("seed started...")
-		if err := database.ApplySeed(db); err != nil {
+		if err := database.ApplySeed(tx); err != nil {
 			logger.LogError(err)
 			os.Exit(1)
 		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
 	}
 }
