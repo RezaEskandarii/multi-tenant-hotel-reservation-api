3c08cdbb292e9b4d2fd01ab08d953405a1bca995
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2022-11-29 21:27:43 +0330

    doc: add postman collection json file

--- pkg/tenant_connection_string_resolver/db_logger.go
+++ pkg/tenant_connection_string_resolver/db_logger.go
@@ -7,13 +7,14 @@ import (
 	"time"
 )
 
+// GetDbLogger returns logger interface
 func GetDbLogger() logger.Interface {
 	return logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold: time.Second,   // Slow SQL threshold
-			LogLevel:      logger.Silent, // Log level
-			Colorful:      true,          // Disable color
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Disable color,
 		},
 	)
 }
