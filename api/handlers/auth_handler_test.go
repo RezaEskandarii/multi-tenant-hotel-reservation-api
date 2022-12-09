047a7c0aae8032b2beb1c1b08d99926b04a2a655
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2022-12-09 19:19:04 +0330

    test: add test to signin handler

---
+++ api/handlers/auth_handler_test.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"reservation-api/internal/commons"
+	"reservation-api/internal/commons/httphelper"
+	"testing"
+	"time"
+)
+
+var (
+	httpReq = httphelper.New()
+	baseUrl = "http://127.0.0.1:8080/api/v1"
+)
+
+func getHeaders() map[string]string {
+
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/json"
+	headers["X-Tenant-ID"] = "1"
+
+	return headers
+}
+
+func TestAuthHandler(t *testing.T) {
+
+	t.Run("test_can_signin", func(t *testing.T) {
+
+		body := make(map[string]interface{})
+
+		body["username"] = "reza"
+		body["password"] = "Reza1234"
+
+		o := commons.JWTTokenResponse{}
+
+		err, _, resp := httpReq.Post(httphelper.Request{
+			Ctx:     context.Background(),
+			Url:     baseUrl + "/auth/signin",
+			Body:    body,
+			Out:     &o,
+			Headers: getHeaders(),
+		})
+
+		assert.Nil(t, err)
+		assert.NotNil(t, resp)
+		assert.Equal(t, resp.StatusCode, http.StatusOK)
+		assert.NotEqual(t, o.AccessToken, "")
+		assert.True(t, len(o.AccessToken) > 0)
+		assert.True(t, o.ExpireAt.After(time.Now()))
+
+	})
+}
